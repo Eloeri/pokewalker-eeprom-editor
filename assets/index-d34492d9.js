a1cbe643e1f87a71f3c618e48c4de7e873d69310
Eloeri <42394653+Eloeri@users.noreply.github.com>
2024-10-24 10:24:16 -0500

    Update index-d34492d9.js

--- assets/index-d34492d9.js
+++ assets/index-d34492d9.js
@@ -1706,7 +1706,7 @@ function on(t, e, n) {
     }
     async function c() {
         n(0, a = !1);
-        const s = await fetch("https://raw.githubusercontent.com/Eloeri/pokewalker-eeprom-editor/gh-pages/eeprom.bin");
+        const s = await fetch("raw.githubusercontent.com/Eloeri/pokewalker-eeprom-editor/main/eeprom.bin");
         r("file", s.arrayBuffer())
     }
     return t.$$set = s => {
